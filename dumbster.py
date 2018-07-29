712c3f8a88943e16508af54ae2dd14a9f887921b
Avinassh3 <38003302+Avinassh3@users.noreply.github.com>
2018-07-29 19:18:59 +0530

    Add files via upload

--- dumbster.py
+++ dumbster.py
@@ -1,7 +1,11 @@
 
 import BattingODI
 import Battingtest
+import BowlingOdi
+
 print('TEST DATA IS PRINTED')
 Battingtest.testbatting('testdata.csv')
 print('ODI DATA IS PRINTED')
 BattingODI.Odibatting('odidata.csv')
+print('ODI BOWLING DATA IS PRINTED')
+BowlingOdi.odiBowling('odibowldata.csv')
--- scrap.py
+++ scrap.py
@@ -6,7 +6,12 @@ test=['http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;spanmax1
       'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=6;template=results;type=batting',
       'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=7;template=results;type=batting',
       'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=8;template=results;type=batting']
+
+
 teams=['England','Australia','SouthAfrica','Westindies','Newzealand','INDIA','Pakistan','Srilanka']
+
+
+
 odi=['http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=1;template=results;type=batting',
      'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=2;template=results;type=batting',
      'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=3;template=results;type=batting',
@@ -15,3 +20,14 @@ odi=['http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=
      'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=6;template=results;type=batting',
      'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=7;template=results;type=batting',
      'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=8;template=results;type=batting',]
+
+
+
+odibowl=['http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=1;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=2;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=3;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=4;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=5;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=6;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=7;template=results;type=bowling',
+         'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;spanmax1=27+Jul+2018;spanmin1=27+Jul+2016;spanval1=span;team=8;template=results;type=bowling',]
